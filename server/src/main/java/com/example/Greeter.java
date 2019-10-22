f8291af0c91acbf9256b099eeb855d3cd3b5dd5b
Rrodelgo <56826644+Rrodelgo@users.noreply.github.com>
2019-10-22 12:04:15 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -4,7 +4,7 @@ package com.example;
  * This is a class.
  */
 public class Greeter {
-
+public long MAGICO = new Long("2333");
   /**
    * This is a constructor.
    */
@@ -16,8 +16,8 @@ public class Greeter {
    * Metodo de greet modificado.
    */
   public String greet(String someone) {
-    Long lalala = MAGICO;
-    if(true){
+    /**Long lalala = MAGICO; */
+    if(MAGICO > 1){
       return String.format("Hello, %s!", someone);
     }
     else{
